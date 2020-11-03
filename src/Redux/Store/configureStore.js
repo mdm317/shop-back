5d7c6e88e57d43c891d643f27645d0deb08c46a7
unknown <mdm317@naver.com>
2020-11-03 20:43:55 +0900

    change redux setting

--- src/Redux/Store/configureStore.js
+++ src/Redux/Store/configureStore.js
@@ -12,7 +12,7 @@ const configureStore = () =>
     // 루트 리듀서를 전달한다
     rootReducer,
 
-    process.env.REACT_APP_DEV==='DEV'?    composeWithDevTools(applyMiddleware(createLogger(),reduxThunk)):composeWithDevTools(applyMiddleware(reduxThunk))
+    process.env.REACT_APP_DEV==='DEV'?    composeWithDevTools(applyMiddleware(createLogger(),reduxThunk)):applyMiddleware(reduxThunk)
     // 미들웨어 형태의 리덕스 개발 도구를 추가한다
     
   )
